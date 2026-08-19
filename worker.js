39fdeb76e82b4d6b5ff53a9f2e65ae5a1a853d92
Venomous49 <marin.iglesias@hotmail.fr>
2026-08-20 01:21:49 +0200

    Deploy visual fixes with fresh asset versions

--- worker.js
+++ worker.js
@@ -1,8 +1,8 @@
 class RiseLooterHead {
   element(element) {
-    element.append('<link rel="stylesheet" href="/creator-hd.css?v=2">', { html: true });
+    element.append('<link rel="stylesheet" href="/creator-hd.css?v=3">', { html: true });
     element.append('<script src="/creator-safe-runtime.js?v=6" defer></script>', { html: true });
-    element.append('<script src="/silhouette-fix.js?v=2" defer></script>', { html: true });
+    element.append('<script src="/silhouette-fix.js?v=3" defer></script>', { html: true });
   }
 }
 export default {
@@ -13,7 +13,7 @@ export default {
     const headers=new Headers(response.headers);
     headers.set('x-riselooter-creator-source','validated');
     headers.set('x-riselooter-creator-runtime','webp-matrix-runtime-v6');
-    headers.set('x-riselooter-silhouettes','exact-master-v2');
+    headers.set('x-riselooter-silhouettes','exact-master-v3');
     return new HTMLRewriter().on('head',new RiseLooterHead()).transform(new Response(response.body,{status:response.status,statusText:response.statusText,headers}));
   }
 };
