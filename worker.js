9110aa8ba05c88dc9347227683ccd25e0554d943
Venomous49 <marin.iglesias@hotmail.fr>
2026-08-20 00:25:56 +0200

    Deploy validated creator runtime v2

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 class RiseLooterHead {
   element(element) {
     element.append('<link rel="stylesheet" href="/creator-hd.css?v=2">', { html: true });
-    element.append('<script src="/creator-safe-runtime.js?v=1" defer></script>', { html: true });
+    element.append('<script src="/creator-safe-runtime.js?v=2" defer></script>', { html: true });
     element.append('<script src="/silhouette-fix.js?v=1" defer></script>', { html: true });
   }
 }
@@ -13,7 +13,7 @@ export default {
     if (!contentType.includes('text/html')) return response;
 
     const headers = new Headers(response.headers);
-    headers.set('x-riselooter-creator-source', 'reviewed-source-runtime-v1');
+    headers.set('x-riselooter-creator-source', 'validated-250-presets-v2');
     headers.set('x-riselooter-silhouettes', 'exact-master-v1');
 
     const html = new Response(response.body, {
