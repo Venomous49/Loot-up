1c55de748a2c5b440e388a43ea7aff0e279b8306
Venomous49 <marin.iglesias@hotmail.fr>
2026-08-20 23:50:22 +0200

    Serve enlarged V4 creator fit

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ class RiseLooterHead {
   element(element) {
     element.append('<link rel="stylesheet" href="/creator-hd.css?v=23">', { html: true });
     element.append('<script src="/creator-cache-v23.js?v=26" defer></script>', { html: true });
-    element.append('<script src="/safe-ui-bootstrap.js?v=layered-v4-clean-source" defer></script>', { html: true });
+    element.append('<script src="/safe-ui-bootstrap.js?v=layered-v4-fit1" defer></script>', { html: true });
   }
 }
 
@@ -16,8 +16,8 @@ export default {
     headers.set('pragma', 'no-cache');
     headers.set('expires', '0');
     headers.set('x-riselooter-creator-source', 'layered-v4-clean-source');
-    headers.set('x-riselooter-creator-version', 'layered-v4-clean-source');
-    headers.set('x-riselooter-runtime-hotfixes', 'safe-bootstrap-layered-v4-clean-source');
+    headers.set('x-riselooter-creator-version', 'layered-v4-fit1');
+    headers.set('x-riselooter-runtime-hotfixes', 'safe-bootstrap-layered-v4-fit1');
     return new HTMLRewriter().on('head', new RiseLooterHead()).transform(new Response(response.body,{status:response.status,statusText:response.statusText,headers}));
   }
 };
