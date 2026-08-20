a025f72fa395395c9880c66177adcd70964fa80b
Venomous49 <marin.iglesias@hotmail.fr>
2026-08-20 23:33:55 +0200

    Serve cleaned V4 creator runtime

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ class RiseLooterHead {
   element(element) {
     element.append('<link rel="stylesheet" href="/creator-hd.css?v=23">', { html: true });
     element.append('<script src="/creator-cache-v23.js?v=26" defer></script>', { html: true });
-    element.append('<script src="/safe-ui-bootstrap.js?v=layered-v3" defer></script>', { html: true });
+    element.append('<script src="/safe-ui-bootstrap.js?v=layered-v4-clean-source" defer></script>', { html: true });
   }
 }
 
@@ -15,9 +15,9 @@ export default {
     headers.set('cache-control', 'no-store, no-cache, must-revalidate, max-age=0');
     headers.set('pragma', 'no-cache');
     headers.set('expires', '0');
-    headers.set('x-riselooter-creator-source', 'layered-v3');
-    headers.set('x-riselooter-creator-version', 'layered-v3');
-    headers.set('x-riselooter-runtime-hotfixes', 'safe-bootstrap-layered-v3');
+    headers.set('x-riselooter-creator-source', 'layered-v4-clean-source');
+    headers.set('x-riselooter-creator-version', 'layered-v4-clean-source');
+    headers.set('x-riselooter-runtime-hotfixes', 'safe-bootstrap-layered-v4-clean-source');
     return new HTMLRewriter().on('head', new RiseLooterHead()).transform(new Response(response.body,{status:response.status,statusText:response.statusText,headers}));
   }
 };
