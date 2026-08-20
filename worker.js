bd01e7993ce0bca02398c4397d7643b176b33b20
Venomous49 <marin.iglesias@hotmail.fr>
2026-08-20 13:45:43 +0200

    Deploy safe creator and silhouette bootstrap

--- worker.js
+++ worker.js
@@ -1,9 +1,9 @@
 class RiseLooterHead {
   element(element) {
-    // Keep only the validated creator assets. Runtime DOM hotfixes are disabled
-    // because their MutationObservers can fight the page renderer and freeze UI.
     element.append('<link rel="stylesheet" href="/creator-hd.css?v=22">', { html: true });
     element.append('<script src="/creator-cache-v23.js?v=23" defer></script>', { html: true });
+    // Deterministic one-shot bootstrap only: no MutationObserver, no render loop.
+    element.append('<script src="/safe-ui-bootstrap.js?v=9" defer></script>', { html: true });
   }
 }
 
@@ -15,7 +15,7 @@ export default {
     const headers = new Headers(response.headers);
     headers.set('x-riselooter-creator-source', 'validated');
     headers.set('x-riselooter-creator-version', 'dedicated-fullbody-v23');
-    headers.set('x-riselooter-runtime-hotfixes', 'disabled');
+    headers.set('x-riselooter-runtime-hotfixes', 'safe-bootstrap-v9');
     return new HTMLRewriter().on('head', new RiseLooterHead()).transform(new Response(response.body,{status:response.status,statusText:response.statusText,headers}));
   }
 };
