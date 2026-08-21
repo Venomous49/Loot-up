55b98ac559ddad3a3c44e1c399cab79d885f37e8
Venomous49 <marin.iglesias@hotmail.fr>
2026-08-21 14:50:05 +0200

    Déployer l’accueil Débutant sans texte incrusté

--- worker.js
+++ worker.js
@@ -3,8 +3,8 @@ class RiseLooterHead {
     element.append('<link rel="stylesheet" href="/creator-hd.css?v=23">', { html: true });
     element.append('<script src="/creator-cache-v23.js?v=27" defer></script>', { html: true });
     element.append('<script src="/safe-ui-bootstrap.js?v=fixed-gender-v1" defer></script>', { html: true });
-    element.append('<script src="/fixed-stage-home.js?v=6" defer></script>', { html: true });
-    element.append('<script src="/site-polish-v3.js?v=6" defer></script>', { html: true });
+    element.append('<script src="/fixed-stage-home.js?v=7" defer></script>', { html: true });
+    element.append('<script src="/site-polish-v3.js?v=7" defer></script>', { html: true });
   }
 }
 
@@ -18,8 +18,8 @@ export default {
     headers.set('pragma', 'no-cache');
     headers.set('expires', '0');
     headers.set('x-riselooter-creator-source', 'layered-v4-clean-source');
-    headers.set('x-riselooter-creator-version', 'fixed-gender-v1-stage-home-v6-pristine-safe');
-    headers.set('x-riselooter-runtime-hotfixes', 'safe-bootstrap-fixed-gender-v1-stage-home-v6-pristine-safe');
+    headers.set('x-riselooter-creator-version', 'fixed-gender-v1-stage-home-v7-clean-debutant');
+    headers.set('x-riselooter-runtime-hotfixes', 'safe-bootstrap-fixed-gender-v1-stage-home-v7-clean-debutant');
     return new HTMLRewriter().on('head', new RiseLooterHead()).transform(new Response(response.body,{status:response.status,statusText:response.statusText,headers}));
   }
 };
