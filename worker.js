a4b2ad5a0e9fb21e406843ca2720adbb0bb3fa05
Venomous49 <marin.iglesias@hotmail.fr>
2026-08-22 16:36:34 +0200

    Bust runtime cache for final super-resolution assets

--- worker.js
+++ worker.js
@@ -2,8 +2,8 @@ class RiseLooterHead {
   element(element) {
     element.append('<link rel="stylesheet" href="/creator-hd.css?v=23">', { html: true });
     element.append('<script src="/creator-cache-v23.js?v=27" defer></script>', { html: true });
-    element.append('<script src="/safe-ui-bootstrap.js?v=superres-x4-v1" defer></script>', { html: true });
-    element.append('<script src="/fixed-stage-home.js?v=superres-x4-v1" defer></script>', { html: true });
+    element.append('<script src="/safe-ui-bootstrap.js?v=superres-x4-v2" defer></script>', { html: true });
+    element.append('<script src="/fixed-stage-home.js?v=superres-x4-v2" defer></script>', { html: true });
     element.append('<script src="/site-polish-v3.js?v=7" defer></script>', { html: true });
   }
 }
@@ -18,8 +18,8 @@ export default {
     headers.set('pragma', 'no-cache');
     headers.set('expires', '0');
     headers.set('x-riselooter-creator-source', 'canonical-stage-images');
-    headers.set('x-riselooter-creator-version', 'superres-x4-v1');
-    headers.set('x-riselooter-runtime-hotfixes', 'superres-x4-v1');
+    headers.set('x-riselooter-creator-version', 'superres-x4-v2');
+    headers.set('x-riselooter-runtime-hotfixes', 'superres-x4-v2');
     return new HTMLRewriter().on('head', new RiseLooterHead()).transform(new Response(response.body,{status:response.status,statusText:response.statusText,headers}));
   }
 };
